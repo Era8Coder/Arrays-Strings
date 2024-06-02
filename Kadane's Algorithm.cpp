6611c7a1cc63a733517c8e1d0163630b44d20428
Code_Masher <137721678+Era8Coder@users.noreply.github.com>
2024-06-02 15:02:45 +0530

    Update Kadane's Algorithm.cpp

--- Kadane's Algorithm.cpp
+++ Kadane's Algorithm.cpp
@@ -1,5 +1,7 @@
 /*
-    Kadane's is the most popular Algorithm to find the MAXIMUM Sum subarray :_
+    Kadane's is the most popular Algorithm to find the MAXIMUM Sum subarray:_
+    The subarray with a negative sum is discarded (by assigning max_ending_here = 0 in code).
+    We carry subarray till it gives a positive sum.
 */
 
 #include<bits/stdc++.h>
